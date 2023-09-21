740ccaff73d816260f3e5d1f75fa623cd30a6b6d
kubusiaq <sactarek@gmail.com>
2023-09-21 22:34:53 +0200

    fixme2.py completed

---
+++ python_in_ctfs/fixme2.py
@@ -0,0 +1,8 @@
+Description
+Fix the syntax error in the Python script to print the flag.
+Download Python script
+
+
+picoCTF{3qu4l1ty_n0t_4551gnm3nt_4863e11b}
+
+i dont even see need to paste code, just changed assignment sign to equality sign (= -> ==)
